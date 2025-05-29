2afed60a0e2f30ccbb35eab149d14673ecd474ac
fernando.salazar <fernando.salazar@rappi.com>
2025-05-29 17:40:58 -0500

    ajustando logos

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,14 @@ const outfit = Outfit({subsets: ["latin"]});
 export const metadata: Metadata = {
   title: "Ribello - Menú Digital",
   description: "Menú Digital del Restaurante Ribello - Bistró & Cocktails",
+  icons: {
+    icon: [
+      { url: '/images/Logos/favicon.ico', sizes: 'any' },
+      { url: '/images/Logos/favicon-16x16.png', sizes: '16x16' },
+      { url: '/images/Logos/favicon-32x32.png', sizes: '32x32' },
+    ],
+    apple: { url: '/images/Logos/apple-touch-icon.png', sizes: '180x180' }
+  }
 };
 
 export default function RootLayout({
